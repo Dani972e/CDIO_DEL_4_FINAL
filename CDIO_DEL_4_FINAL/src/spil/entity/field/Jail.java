1a0e05df87551e023a53fb54114848b73e47e1f6
Daniel Larsen <iyyel@users.noreply.github.com>
2017-01-05 12:46:49 +0100

    Update Jail.java

--- CDIO_DEL_4_FINAL/src/spil/entity/field/Jail.java
+++ CDIO_DEL_4_FINAL/src/spil/entity/field/Jail.java
@@ -1,20 +1,30 @@
 package spil.entity.field;
 
 import spil.boundary.GUIBoundary;
+import spil.entity.JailedPlayers;
 import spil.entity.Player;
 import spil.entity.TextInfo;
 
 public final class Jail extends Field {
 
+	private JailedPlayers jailedPlayers;
+	private boolean isJail;
+
+	public Jail(JailedPlayers jailedPlayers, boolean isJail) {
+		this.jailedPlayers = jailedPlayers;
+		this.isJail = isJail;
+	}
+
 	@Override
 	public void landOnField(Player player) {
-		if (player.getPosition() == 10) {
+		if (!isJail) {
 			GUIBoundary.print(TextInfo.jailFieldMessage(player));
-		} else if (player.getPosition() == 30) {
+		} else if (isJail) {
 			GUIBoundary.removePlayerCar(player);
 			player.setPosition(10);
 			GUIBoundary.placePlayerCar(player);
 			GUIBoundary.print(TextInfo.jailedFieldMessage(player));
+			jailedPlayers.jailPlayer(player);
 		}
 	}
 
